77bacd750f98ac8a14b2d66d77386d9dba60b16a
Paul Desmond Parker <pauldesmondparker@gmail.com>
2019-09-02 13:03:58 +0800

    Add ability to process multiple codes per line item
    Populate coupons from actual fetched data
    Reorganise directory structure
    Remove unused templates
    Add css for tables in view-order and email
    Increment request no. on each request
    Add recovery for error "Order already exist!"
    Changed Staging enable to Live enable
    Remove unused billing_email
    Fix X-API-KEY typo
    Add errors to generated order note

--- wc_indulgemall_coupons.php
+++ wc_indulgemall_coupons.php
@@ -19,15 +19,16 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
 
     //magic function (triggered on initialization)
     public function __construct(){
-    //    add_action('admin_enqueue_scripts', array($this,'enqueue_admin_scripts_and_styles')); //admin scripts and styles
-    //    add_action('wp_enqueue_scripts', array($this,'enqueue_public_scripts_and_styles')); //public scripts and styles
+      add_action('wp_enqueue_scripts', array($this,'enqueue_public_scripts_and_styles')); //public scripts and styles
       add_action( 'plugins_loaded', 'wcimc_register_indulge_coupon_type' ); // Included with inc/wc-product-indulge-coupon.php
       add_filter( 'product_type_selector', array($this, 'wcimc_add_indulge_coupon_type') );
 //      add_action( 'woocommerce_payment_complete', array($this, 'fetch_coupon_and_process'));
       add_action( 'woocommerce_order_status_completed', array($this, 'fetch_coupon_and_process'));
       add_action( 'woocommerce_single_product_summary', array($this, 'indulge_coupon_template'), 60 );
+      add_action( 'woocommerce_order_item_meta_end', array($this, 'order_item_meta_end'), 10, 4 );
 
       if ( is_admin() ){ // admin actions
+        add_action('admin_enqueue_scripts', array($this,'enqueue_admin_scripts_and_styles')); //admin scripts and styles
         add_action( 'woocommerce_product_options_general_product_data', function(){
             echo '<div class="options_group show_if_indulge_coupon clear"></div>';
         } );
@@ -36,38 +37,21 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
         add_action( 'admin_menu', array($this, 'register_indulge_api_settings_submenu_page')); // Place admin sub menu
         add_action( 'admin_init', array($this, 'register_indulgemall_settings')); // Whitelist options settings
 
-//        add_filter( 'product_type_options', array($this, 'add_indulge_coupon_product_option'));  // Add "Indulge Coupon" checkbox, similar to "Virtual" on products
         add_action( 'woocommerce_admin_process_product_object', array($this, 'save_indulge_coupon_product_option')); // Save value of "Indulge Coupon" checkbox
         add_action( 'woocommerce_product_data_panels', array($this, 'add_indulge_coupon_tab_options') );
         add_action( 'admin_head', array($this, 'wcpp_custom_style'));
 
         add_filter( 'woocommerce_product_data_tabs', array($this, 'indulge_sku_settings_tab'));
         add_action( 'woocommerce_admin_process_product_object', array($this, 'save_indulge_coupon_tab_options'), 10, 1);
-        add_filter( 'woocommerce_hidden_order_itemmeta', array($this, 'add_indulge_coupon_code_to_hidden_order_itemmeta'), 10, 1 );
+        add_filter( 'woocommerce_hidden_order_itemmeta', array($this, 'add_indulge_coupon_codes_to_hidden_order_itemmeta'), 10, 1 );
       } else {
         // non-admin enqueues, actions, and filters
-        add_action( 'woocommerce_order_item_meta_end', array($this, 'order_item_meta_end'), 10, 4 );
-        //add_filter( 'woocommerce_locate_template', array($this, 'override_wc_locate_template'), 10, 3 );  // Override wc_locate_template result from WC so we can override WC templates
       }
 
     }
 
     //triggered on activation of the plugin (called only once)
     public static function plugin_activate() {
-      // Did this default value elsewhere
-      /*
-      $options = get_option('indulge_api_settings');
-      if ( isset($options['last_request_id']) ) {
-          //error_log( "checking is_int" );
-        if ( ! is_int( $options['last_request_id'] ) ) {
-          $options['last_request_id'] = 0;
-          update_option('indulge_api_settings', $options);
-        }
-      } else {
-        error_log( print_r( "Got problem with plugin_activate, indulge_api_settings not registered yet.", true ) );
-      }
-       */
-      //    $this->register_location_content_type();
         //flush permalinks
        //   flush_rewrite_rules();
     }
@@ -83,10 +67,16 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
     }
 
     public static function plugin_uninstall(){
-      // WARNING: Destructive. All settings will be lost, even if re-installed. Edit: Not true
       unregister_setting( 'indulgemall-option-group', 'indulge_api_settings' );
       // TODO: examine if there are other things that need to be cleaned up here
       //   i.e. Custom Type "Indulge Coupon"
+    }
+
+    public function enqueue_admin_scripts_and_styles() {
+      wp_enqueue_style( 'wcimc_CSS', plugins_url( '/admin/css/wcimc.css', __FILE__ ) );
+    }
+    public function enqueue_public_scripts_and_styles() {
+      wp_enqueue_style( 'wcimc_CSS', plugins_url( '/admin/css/wcimc.css', __FILE__ ) );
     }
 
     public function register_indulge_api_settings_submenu_page() {
@@ -98,8 +88,8 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
       }
       echo '<div class="wrap">';
-      echo '<img src="' . plugin_dir_url(__FILE__) . 'assets/IMall-logo.png">';
-      echo '<h3>Indulge Mall API Settings</h3>';
+      echo '<img src="' . plugin_dir_url(__FILE__) . 'admin/images/IMall-logo.png">';
+      echo '<h1>Indulge Mall API Settings</h1>';
       echo '<form method="post" action="options.php">';
         settings_fields( 'indulgemall-option-group' );
         do_settings_sections( 'indulge-api-settings' );
@@ -117,10 +107,10 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
        */
       register_setting( 'indulgemall-option-group', 'indulge_api_settings' ); //, array($this, 'api_settings_validate'));
 
-      add_settings_section('general_settings', 'General Settings', array($this, 'general_settings_html_callback'), 'indulge-api-settings');
+      add_settings_section('general_settings', 'Common Settings', array($this, 'general_settings_html_callback'), 'indulge-api-settings');
       add_settings_section('staging_settings', 'Staging Settings', array($this, 'staging_settings_html_callback'), 'indulge-api-settings');
-      add_settings_section('live_settings', 'Live Settings', array($this, 'live_settings_html_callback'), 'indulge-api-settings');
-      add_settings_section('select_staging', 'Activate Staging', array($this, 'activate_staging_html_callback'), 'indulge-api-settings');
+      add_settings_section('live_settings', 'Live', array($this, 'live_settings_html_callback'), 'indulge-api-settings');
+      add_settings_section('select_live', 'Go live!', array($this, 'activate_staging_html_callback'), 'indulge-api-settings');
 
       add_settings_field('request_id_prefix', 'Request ID Prefix', array($this, 'request_id_prefix_input_callback'), 'indulge-api-settings', 'general_settings');
       add_settings_field('last_request_id', 'Last Request ID No.', array($this, 'last_request_id_input_callback'), 'indulge-api-settings', 'general_settings');
@@ -135,14 +125,14 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
       add_settings_field('live_account_id', 'Live Account ID', array($this, 'live_accountid_input_callback'), 'indulge-api-settings', 'live_settings');
       add_settings_field('live_password', 'Live Password', array($this, 'live_password_input_callback'), 'indulge-api-settings', 'live_settings');
 
-      add_settings_field('select_credentials', 'Enable', array($this, 'select_input_callback'), 'indulge-api-settings', 'select_staging');
+      add_settings_field('select_credentials', 'Enable', array($this, 'select_input_callback'), 'indulge-api-settings', 'select_live');
     }
 
     function staging_settings_html_callback() {
-      echo '<p>Staging Settings</p>';
+      //echo '<p>Staging Settings</p>';
     }
     function live_settings_html_callback() {
-      echo '<p>Live Settings</p>';
+      //echo '<p>Live Settings</p>';
     }
     function general_settings_html_callback() {
       $options = get_option('indulge_api_settings');
@@ -152,10 +142,9 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
           update_option('indulge_api_settings', $options);
         }
       }
-      echo '<p>Last Request ID No.: ' . $options['last_request_id'] . '</p>';
     }
     function activate_staging_html_callback() {
-      echo '<p>Activate Staging?</p>';
+      //echo '<p>Activate Live?</p>';
     }
 
     function request_id_prefix_input_callback() {
@@ -177,7 +166,6 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
 
     function staging_apikey_input_callback() {
       $options = get_option('indulge_api_settings');
-      //error_log( "Test: " . print_r( $options, true ) );
       echo "<input id='staging_api_key' name='indulge_api_settings[staging_api_key]' size='40' type='text' value='{$options['staging_api_key']}' />";
     }
 
@@ -220,22 +208,6 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
       }
         echo "<input id='select_credentials' name='indulge_api_settings[select_credentials]' type='checkbox' value='1' " . checked( $checked, 1, false) . "/>";
     }
-
-    /**
-     * Add 'Indulge Coupon' product option
-     */
-    /*
-    function add_indulge_coupon_product_option( $product_type_options ) {
-      $product_type_options['enable_indulge_coupon'] = array(
-        'id'            => '_enable_indulge_coupon',
-        'wrapper_class' => 'show_if_virtual',
-        'label'         => __( 'Indulge Coupon', 'woocommerce' ),
-        'description'   => __( 'Activates Indulge Mall API to fetch coupon code on payment success.', 'woocommerce' ),
-        'default'       => 'no'
-      );
-      return $product_type_options;
-    }
-     */
 
     /**
      * Add Indulge Mall product tab.
@@ -288,7 +260,7 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
     function fetch_coupon_and_process( $order_id ){
       $options = get_option('indulge_api_settings');
       $order = wc_get_order( $order_id );
-      $billingEmail = $order->get_billing_email();
+      //$billingEmail = $order->get_billing_email();
       $order_items = $order->get_items();
       $nextRequestId = $options['last_request_id'] + 1;
 
@@ -297,30 +269,27 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
       // #################
         // Staging or live: from Woocommerce -> Indulge Mall settings
       if ( isset($options['select_credentials']) && $options['select_credentials']) {
-        // Staging
-        $url = $options['staging_endpoint'];
-        $apiKey = $options['staging_api_key'];
-        $account = $options['staging_account_id'];
-        $password = $options['staging_password'];
-      } else {
         // Live
         $url = $options['live_endpoint'];
         $apiKey = $options['live_api_key'];
         $account = $options['live_account_id'];
         $password = $options['live_password'];
+      } else {
+        // Staging
+        $url = $options['staging_endpoint'];
+        $apiKey = $options['staging_api_key'];
+        $account = $options['staging_account_id'];
+        $password = $options['staging_password'];
       }
-      error_log( "URL: " . $url . ";  apiKey: " . $apiKey . ";  account: " . $account . ";  password: " . $password );
 
       if ( !is_wp_error( $order_items ) ) {
         foreach( $order_items as $item_id => $order_item ) {
           $product = $order_item->get_product();
-  //        error_log( "Product: " . print_r($product, true));
           if ($product && "indulge_coupon" === $product->get_type()) {
             $productSku = $product->get_meta( '_indulge_sku', true );
             $productQty = $order_item->get_quantity();
             $requestId = $options['request_id_prefix'] . $nextRequestId;
             $headers = array("X-Api_Key" => $apiKey, "Content-type" => "application/json;charset=UTF-8");
-            error_log("SKU: " . $productSku . "  Qty: " . $productQty . "  RequestId: " . $requestId . "  Headers: " . print_r($headers, true));
 
             $arrayOfParameters = array(
               'account' => $account
@@ -336,20 +305,30 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
               'redirection' => 5,
               'httpversion' => '1.0',
               'blocking' => true,
-              'headers' => array("X-Api_Key" => $apiKey, "Content-type" => "application/json;charset=UTF-8"),
+              'headers' => array("X-Api-Key" => $apiKey, "Content-type" => "application/json;charset=UTF-8"),
               'body' => json_encode( $arrayOfParameters ),
               'cookies' => array()
               )
             );
-//            if ( !is_wp_error( $response ) ) {
-              //error_log( "Response: " . print_r($response, true));
- //           }
-            // Fake it! As though we successfully fetched coupon
-            $note = __("Coupon successfully fetched from Indulge Mall.");
-            //$note = __("There was an error: " . $response);
-            $order->add_order_note( $note );
-            $coupon = 'X43363';
-            wc_add_order_item_meta($item_id, '_indulge_coupon_code', $coupon, true ); 
+            $body = json_decode($response['body'], true);
+            if ( !$body['status'] ) {
+              if ( $body['message'] === "Order already exist!" ) {
+                $options['last_request_id'] = $nextRequestId;
+                update_option('indulge_api_settings', $options);
+                $this->fetch_coupon_and_process( $order_id );
+              } else {
+                $note = __("Error: ") . print_r($response['body'], true);
+                $order->add_order_note( $note );
+              }
+            } else {
+              $note = __("Coupon successfully fetched from Indulge Mall.");
+              $order->add_order_note( $note );
+              $coupons = json_encode($body['data']['products']);
+              wc_add_order_item_meta($item_id, '_indulge_coupon_codes', $coupons, true ); 
+
+              $options['last_request_id'] = $nextRequestId;
+              update_option('indulge_api_settings', $options);
+            }
           }
         }
       }
@@ -357,30 +336,21 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
 
     /**
      * Add content to each 'indulge_coupon' product within email
-     * E.g. Coupon code: <b>XXXXXXX</b>
      */
     function order_item_meta_end( $item_id, $item, $order, $plain_text ){
       $order_status = $order->get_status();
       if ($order_status === 'completed') {
         $product = $item->get_product();
         if ($product && "indulge_coupon" === $product->get_type()) {
-          $coupon_code = wc_get_order_item_meta( $item_id, '_indulge_coupon_code');
-          echo '<p> Coupon code: <b>' . $coupon_code . '</b></p>';
+          $coupon_codes = json_decode(wc_get_order_item_meta( $item_id, '_indulge_coupon_codes'), true);
+          echo '<table class="wcimc_table"><thead><tr><th>SKU</th><th>Code</th><th>Expiry</th></tr></thead><tbody>';
+          foreach( $coupon_codes as $coupon ) {
+            echo '<tr><td>' . $coupon['sku'] . '</td><td><b>' . $coupon['code'] . '</b></td><td>' . $coupon['expiry'] . '</td></tr>';
+          }
+          echo '</tbody></table>';
         }
       }
     }
-
-    /*
-    function api_settings_validate($input) {
-      // Obviously, not QUITE right
-      $options = get_option('indulge-api-settings');
-      $options['text_string'] = trim($input['text_string']);
-      if(!preg_match('/^[a-z0-9]{32}$/i', $options['text_string'])) {
-        $options['text_string'] = '';
-      }
-      return $options;
-    }
-     */
 
     /*
      * Add custom product type: Indulge Coupon to product types
@@ -392,11 +362,11 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
     }
 
     /*
-     * Add _indulge_coupon_code meta key to list of hidden items in order itemmeta
+     * Add _indulge_coupon_codes meta key to list of hidden items in order itemmeta
      * Admin Edit Order
      */
-    function add_indulge_coupon_code_to_hidden_order_itemmeta( $array ) {
-        array_push($array, '_indulge_coupon_code');
+    function add_indulge_coupon_codes_to_hidden_order_itemmeta( $array ) {
+        array_push($array, '_indulge_coupon_codes');
         return $array; 
     }
 
@@ -424,17 +394,6 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
         wc_get_template( 'single-product/add-to-cart/simple.php' );
       }
     }
-    /**
-     * Override the result of wc-core-functions.php::wc_locate_template
-     * From version 3.3.0
-     */
-    function override_wc_locate_template( $template, $template_name, $template_path ) {
-      $wcimc_plugin_path  = untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/woocommerce/';
-      if ( file_exists( $wcimc_plugin_path . $template_name ) ) {
-        $template = $wcimc_plugin_path . $template_name;
-      }
-      return $template;
-    }
 
     public function enable_js_on_wc_product() {
       global $post, $product_object;
@@ -457,7 +416,7 @@ if ( ! class_exists( 'WC_Indulgemall_Coupons' ) ) {
      }
   }
 
-  include(plugin_dir_path(__FILE__) . 'inc/wc-product-indulge-coupon.php');
+  include(plugin_dir_path(__FILE__) . 'includes/wc-product-indulge-coupon.php');
   new WC_Indulgemall_Coupons();
 }
 
